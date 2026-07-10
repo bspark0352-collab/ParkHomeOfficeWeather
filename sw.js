4692c6ed00106d9456ce462f27b56650e8ff6d86
bspark0352-collab <bspark0352@gmail.com>
2026-07-10 17:53:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'commute-weather-v8';
+const CACHE_NAME = 'commute-weather-v9';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
